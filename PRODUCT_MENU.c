d9702d011396034d112816a7463b140b7863ef8e
Harshaday04 <harshada.yenketeramdoo1@umail.uom.ac.mu>
2024-10-01 22:23:56 +0400

    The menu for product management

---
+++ PRODUCT_MENU.c
@@ -0,0 +1,11 @@
+#ifndef PRODUCT_MENU_H
+#define PRODUCT_MENU_H
+
+#include "PRODUCT_M.h"         // Include the main product structure and management functions
+#include "PRODUCT_filtering.h" // Include filtering functionality
+#include "PRODUCT_Sorting.h"   // Include sorting functionality
+
+// Function to display and handle the main menu
+void displayMenu(Product products[], int *count);
+
+#endif
